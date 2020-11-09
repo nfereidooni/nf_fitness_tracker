d8fd72dd47334ae93c0721d9461d95234dba409c
Niki Fereidooni <nikifereidooni@gmail.com>
2020-11-08 21:13:51 -0500

    deploying

--- ap_copied/orm/index.js
+++
@@ -1,9 +0,0 @@
-// include mongoose models (it will include each file in the models directory)
-let orm = {};
-const ormModules = ['users','images'];
-ormModules.forEach( function( module ){
-    orm[module] = require(`${__dirname}/${module}.js`);
-});
-
-
-module.exports = orm;
