b1ec3b72d890489a73d8ca5b9f594be67ff2c146
Niki Fereidooni <nikifereidooni@gmail.com>
2020-11-13 16:34:51 -0500

    deploying

---
+++ models/workout/index.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const workoutSchema = new mongoose.Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise type."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise name."
+            },
+            duration: {
+                type: Number,
+                required: "Enter the exercise duration (minutes)."
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-require( 'dotenv' ).config(); // looks for .env ; process.env gets it's values
+// require( 'dotenv' ).config(); // looks for .env ; process.env gets it's values
 
 const PORT = process.env.PORT || 8080;
 
 // dependencies & setup
 const express = require('express');
-const apiRouter = require('./ap_copied/router');
+const mongoose = require('mongoose');
+// const apiRouter = require('./ap_copied/router');
 const app = express();
+
 
 // for parsing incoming POST data
 app.use(express.urlencoded({ extended: true }));
@@ -15,8 +17,17 @@ app.use(express.json());
 app.use( express.static('public') );
 
 // for routes
-apiRouter(app);
+// apiRouter(app);
 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitness_db',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 // == process-wide error handling
 process.on('uncaughtException', err => {
@@ -24,6 +35,16 @@ process.on('uncaughtException', err => {
     process.exit(1); //mandatory (as per the Node.js docs)
 });
 
+// routes
+app.use(require("./routes/html_routes.js"));
+app.use(require("./routes/api_routes.js"));
+
 app.listen(PORT, function() {
-    console.log( `Serving steamy pictures (database: ${process.env.DB_NAME}) on: https://localhost:${PORT}` );
+    console.log( `App on (database: ${process.env.DB_NAME}) on: https://localhost:${PORT}` );
 });
+
+
+
+
+
+
